9105e9e108f5bf6ce30641629f5566db3f7ff8b3
thangbodoiqua <dluy2406@gmail.com>
2024-10-29 21:44:01 +0700

    add test file (src in nodejs.org)

--- blog/src/main.js
+++ blog/src/main.js
@@ -1,9 +1,6 @@
 const express = require('express')
-const morgan = require('morgan')
 const app = express()
-const port = 3000
-
-app.use(morgan('combined'))
+const port = 300
 
 app.get('/trangchu', (req, res) => {
   res.send('<h2 style = "color: blue"> Hello World! </h2>')
