e0111a13de0346edd0903cdacb8ecbc6ff4a3bd6
thangbodoiqua <dluy2406@gmail.com>
2024-10-30 10:24:53 +0700

    add handlebar, nodemon, morgan make layout example

--- blog/src/main.js
+++ blog/src/main.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 300
+const express = require('express');
+const morgan = require('morgan');
+const {engine} = require('express-handlebars');
+const e = require('express');
+const path = require('path');
 
-app.get('/trangchu', (req, res) => {
-  res.send('<h2 style = "color: blue"> Hello World! </h2>')
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+//template engine
+app.engine('hbs', engine({
+  extname: '.hbs'
+})); 
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views')); // Đảm bảo có thư mục views
+app.get('/', (req, res) => {
+  return res.render('home');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app running at http://localhost:${port}`)
 })
